55b54e499f589a2178e70e252f41a05887912c39
userstar <userstar713@gmail.com>
2018-11-20 00:30:17 +0800

    complete commit

--- startup_news/loaders.py
+++ startup_news/loaders.py
@@ -2,21 +2,23 @@ from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Join
 
 
+
+
 class TechCrunchArticleLoader(ItemLoader):
-    default_input_processor = MapCompose(lambda s: unicode(s,  "utf-8"), unicode.strip)
+    default_input_processor = MapCompose(lambda s: str(s,  "utf-8"), str.strip)
     default_output_processor = Join()
 
-    title_in = MapCompose(unicode.strip, unicode.title)
+    title_in = MapCompose(str.strip, str.title)
     title_out = Join()
 
-    text_in = MapCompose(unicode.strip)
+    text_in = MapCompose(str.strip)
     text_out = Join()
 
-    tags_in = MapCompose(unicode.strip)
+    tags_in = MapCompose(str.strip)
     tags_out = Join(separator=u'; ')
 
 class RecodeArticleLoader(TechCrunchArticleLoader):
-    subtitle_in = MapCompose(unicode.strip)
+    subtitle_in = MapCompose(str.strip)
     subtitle_out = Join()
 
 class VentureBeatArticleLoader(TechCrunchArticleLoader):
